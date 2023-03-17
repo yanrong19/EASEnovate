bbde72d95532d87cb20c8d467f9bd1de635c609c
zeentan22 <e0725293@u.nus.edu>
2023-03-17 15:04:07 +0800

    create sidebar

--- src/Views/Home.vue
+++ src/Views/Home.vue
@@ -1,10 +1,13 @@
 <template>
-  <div>Home</div>
+  <SideBar :menuList="menu" />
+  <!-- <div>Home testing</div> -->
   <div v-if="dataLoaded">
-    <div>{{ name }}</div>
-    <div>{{ usertype }}</div>
+    <!-- <div>{{ name }}</div>
+    <div>{{ usertype }}</div> -->
   </div>
   <div v-else>Loading</div>
+  <div style="height: 100vh"></div>
+  <div style="height: 100vh"></div>
 </template>
 
 <script>
@@ -12,6 +15,7 @@ import { getDoc, doc } from "@firebase/firestore";
 import firebaseApp from "../firebase";
 import { getAuth, onAuthStateChanged } from "@firebase/auth";
 import { getFirestore } from "@firebase/firestore";
+import SideBar from "../components/SideBar.vue";
 
 export default {
   data() {
@@ -19,6 +23,18 @@ export default {
       name: "",
       usertype: "",
       dataLoaded: false,
+      menu: [
+        {
+          name: "Home",
+          url: "/",
+          id: 1,
+        },
+        {
+          name: "Home2",
+          url: "/",
+          id: 2,
+        },
+      ],
     };
   },
   methods: {
@@ -52,5 +68,6 @@ export default {
   unmounted() {
     console.log("unmounted");
   },
+  components: { SideBar },
 };
 </script>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,96 @@
+<template>
+  <aside :class="expanded">
+    <div class="menu-toggle-wrap">
+      <button class="menu-toggle" @click="toggleMenu">
+        <span>
+          <SideBarIcon />
+        </span>
+      </button>
+    </div>
+
+    <div :id="expanded" class="menu">
+      <div class="link-to-page" v-for="item in menuList" :key="item.id">
+        <router-link :to="item.url">
+          <div>{{ item.name }}</div>
+        </router-link>
+      </div>
+    </div>
+  </aside>
+</template>
+
+<script>
+import SideBarIcon from "./icons/SideBarIcon.vue";
+export default {
+  name: "SideBar",
+  data() {
+    return {
+      expanded: "not_expanded",
+    };
+  },
+  methods: {
+    toggleMenu() {
+      console.log(this.expanded);
+      if (this.expanded === "is_expanded") {
+        this.expanded = "not_expanded";
+      } else {
+        this.expanded = "is_expanded";
+      }
+    },
+  },
+  props: ["menuList"],
+  components: { SideBarIcon },
+};
+</script>
+
+<style scoped>
+button {
+  border: none;
+  cursor: pointer;
+  background-image: url("../assets/menu.png");
+  background-size: contain;
+  width: 2rem;
+  height: auto;
+}
+a {
+  text-decoration: none;
+}
+.link-to-page:hover {
+  background-color: #3aafa9;
+}
+
+.menu-toggle-wrap {
+  margin-left: 1rem;
+  margin-top: 1rem;
+}
+
+.is_expanded {
+  position: fixed;
+  width: 10%;
+  height: 100vh;
+  background-color: #515a5a;
+  transition: 0.5s ease-in-out;
+}
+
+.link-to-page {
+  display: flex;
+  margin-top: 2rem;
+  background-color: aliceblue;
+  border-right-color: #515a5a;
+  border-right-width: 1px;
+  border-right-style: solid;
+  height: 2rem;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  transition: 0.2s ease-in-out;
+  cursor: pointer;
+}
+
+/* .is_expanded > .menu {
+  display: true;
+} */
+
+#not_expanded {
+  display: none;
+}
+</style>
