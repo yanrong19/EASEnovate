4185b9225ef7cdabd94de44d37d3b8c070a83505
muhdjabir <e0725438@u.nus.edu>
2023-04-11 00:12:40 +0800

    Changed homepage sidebar to reflect user info, connected review to job request

--- src/Views/LeaveReview.vue
+++ src/Views/LeaveReview.vue
@@ -1,9 +1,3 @@
-<template>
-    <div class="mainContainer">
-        <ReviewForm />
-    </div>
-</template>
-
 <script>
     import ReviewForm from "../components/ReviewForm.vue";
     export default {
@@ -14,7 +8,23 @@
         destroyed: function () {
             document.body.style.backgroundColor = null;
         },
+        data() {
+            return {
+                requestID: "",
+                dataLoaded: false,
+            };
+        },
+        async mounted() {
+            this.requestID = this.$route.params.requestID;
+            console.log(this.requestID, "View");
+            this.dataLoaded = true;
+        },
     };
 </script>
+<template>
+    <div class="mainContainer" v-if="dataLoaded">
+        <ReviewForm :requestID="requestID" />
+    </div>
+</template>
 
 <style scoped></style>
--- src/components/ReviewForm.vue
+++ src/components/ReviewForm.vue
@@ -15,16 +15,30 @@
                 <v-spacer />
             </v-card-actions>
             <v-card-item>
-                <v-card-title> Details </v-card-title>
-                <v-sheet rounded border="lg opacity-12" color="grey-lighten-3">
+                <v-card-title> Interior Designer </v-card-title>
+                <v-card-subtitle> {{ idName }} </v-card-subtitle>
+            </v-card-item>
+            <v-card-item>
+                <v-card-title> Description </v-card-title>
+                <v-sheet rounded border="lg opacity-8" color="grey-lighten-3">
                     <p class="mb-8">
-                        This business determines the use of personal data
-                        collectied on our media properties and across the
-                        internet. We may collect data that you submit to us
-                        directly or data that we collect automatically including
-                        from cookies (such as device information or IP address).
-                    </p></v-sheet
-                >
+                        {{ description }}
+                    </p>
+                </v-sheet>
+            </v-card-item>
+            <v-card-item>
+                <v-card-title> Service Provided </v-card-title>
+                <div id="list">
+                    <v-card-actions>
+                        <v-chip
+                            color="teal"
+                            v-for="item in services"
+                            size="large"
+                        >
+                            {{ item }}
+                        </v-chip>
+                    </v-card-actions>
+                </div>
             </v-card-item>
             <v-form>
                 <v-card-item>
@@ -37,16 +51,41 @@
                 </v-card-actions>
 
                 <v-card-item>
-                    <v-card-title> Write a review: </v-card-title>
+                    <v-card-title v-if="this.status == 'Completed'">
+                        Write a review
+                    </v-card-title>
+                    <v-card-title v-else> Your Review </v-card-title>
                 </v-card-item>
                 <v-container>
-                    <v-textarea bg-color="grey-lighten-3"> </v-textarea
-                ></v-container>
+                    <v-textarea
+                        bg-color="grey-lighten-3"
+                        v-model="review"
+                        v-if="this.status == 'Completed'"
+                    >
+                    </v-textarea
+                    ><v-sheet
+                        rounded
+                        border="lg opacity-8"
+                        color="grey-lighten-3"
+                        v-else
+                    >
+                        <p class="mb-8">
+                            {{ description }}
+                        </p>
+                    </v-sheet>
+                </v-container>
                 <v-card-actions>
                     <v-spacer />
-                    <v-btn @click.native="goProfile">
+                    <v-btn
+                        @click.native="updateReview"
+                        v-if="this.status == 'Completed'"
+                    >
                         Submit Review
                         <v-icon end icon="mdi-pencil-box-multiple"></v-icon
+                    ></v-btn>
+                    <v-btn @click.native="goRequest" v-else>
+                        Back to Job Requests
+                        <v-icon end icon="mdi-arrow-left"></v-icon
                     ></v-btn>
                     <v-spacer />
                 </v-card-actions>
@@ -55,22 +94,71 @@
     </div>
 </template>
 <script>
+    import firebaseApp from "../firebase.js";
+    import { getFirestore } from "firebase/firestore";
+    import { getAuth, onAuthStateChanged } from "firebase/auth";
+    import {
+        collection,
+        getDocs,
+        doc,
+        updateDoc,
+        getDoc,
+    } from "firebase/firestore";
+
+    const db = getFirestore(firebaseApp);
     export default {
+        props: ["requestID"],
         data() {
             return {
                 jobID: "",
-                uID: "",
-                cID: "",
+                custName: "",
+                idName: "",
+                idEmail: "",
                 description: "",
-                status: "",
+                services: [],
                 rating: 3,
                 review: "",
+                status: "",
+                dataLoaded: false,
             };
         },
         methods: {
-            goProfile() {
-                this.$router.push("/profile");
+            goRequest() {
+                this.$router.push("/jobrequest");
             },
+
+            async updateReview() {
+                await updateDoc(doc(db, "Job Requests", this.requestID), {
+                    Rating: this.rating,
+                    Review: this.review,
+                    Status: "Reviewed",
+                });
+                this.goRequest();
+            },
+
+            async getJob() {
+                const db = getFirestore(firebaseApp);
+                const docRef = doc(db, "Job Requests", this.requestID);
+                const docSnap = await getDoc(docRef);
+                if (docSnap.exists()) {
+                    console.log(docSnap.data());
+                    this.description = docSnap.data().Details;
+                    this.jobID = this.requestID;
+                    this.idName = docSnap.data().DesignerName;
+                    this.custName = docSnap.data().CustomerName;
+                    this.services = docSnap.data().Services;
+                    this.status = docSnap.data().Status;
+                    this.idEmail = docSnap.data().DesignerEmail;
+                }
+            },
+        },
+        created() {
+            console.log(this.requestID, "cREATED");
+        },
+        mounted() {
+            console.log(this.requestID, "Mounted");
+            this.getJob();
+            this.dataLoaded = true;
         },
     };
 </script>
