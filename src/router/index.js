36f673efa31911a87e79e38bbcfcf62969e1427e
muhdjabir <e0725438@u.nus.edu>
2023-04-10 12:53:33 +0800

    Removed redundant component imports from index and added editid button

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import Start from "../Views/Start.vue";
 import Home from "../Views/Home.vue";
 import Landing from "../Views/Landing.vue";
-import CustomerProfile from "../Views/CustomerProfile.vue";
 import EditProfile from "../Views/EditProfile.vue";
 import DisplayProfile from "../views/DisplayProfile.vue";
-import DisplayID from "@/views/DisplayID.vue";
-import EditID from "@/views/EditID.vue";
 import JobRequest from "@/views/JobRequest.vue";
 import LeaveReview from "@/views/LeaveReview.vue";
 
@@ -30,25 +27,6 @@ const routes = [
       requiresAuth: true, // Indicates that this route requires authentication
     },
   },
-  {
-    path: "/displayid",
-    name: "DisplayID",
-    component: DisplayID,
-  },
-  {
-    path: "/editid",
-    name: "EditID",
-    component: EditID,
-  },
-  /*
-  {
-    path: "/profile",
-    name: "Profile",
-    component: CustomerProfile,
-    meta: {
-      requiresAuth: true,
-    },
-  },*/
   {
     path: "/profile",
     name: "DisplayProfile",
@@ -133,6 +111,27 @@ const router = createRouter({
     path: "/profile/jobrequest",
     name: "jobrequest",
     component: JobRequest,
+    meta: {
+      requiresAuth: true,
+    },
+  },*/
+
+  /*
+  {
+    path: "/displayid",
+    name: "DisplayID",
+    component: DisplayID,
+  },
+  {
+    path: "/editid",
+    name: "EditID",
+    component: EditID,
+  },
+  
+  {
+    path: "/profile",
+    name: "Profile",
+    component: CustomerProfile,
     meta: {
       requiresAuth: true,
     },
